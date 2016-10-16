966c9d05ac40a48b75f6c6eb080432a2893a48ac
Summer <peishan1216@gmail.com>
2016-10-16 11:53:50 +0800

    18 練習透過 Entity Framework 更新資料與批次更新多筆資料

---
+++ MVC5Course/Controllers/EFController.cs
@@ -0,0 +1,77 @@
+﻿using MVC5Course.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5Course.Controllers
+{
+    public class EFController : Controller
+    {
+        FabricsEntities db = new FabricsEntities();
+        // GET: EF
+        public ActionResult Index()
+        {
+            var db = new FabricsEntities();
+            var data = db.Product.Where(p => p.ProductName.Contains("White"));
+            return View(data);
+        }
+        public ActionResult Creat()
+        {
+            var product = new Product()
+            {
+                ProductName = "White Cat",
+                Active = true,
+                Price = 100,
+                Stock = 5
+            };
+
+            db.Product.Add(product);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var product = db.Product.Find(id);
+            db.Product.Remove(product);
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+
+
+        }
+
+        public ActionResult Details(int id)
+        {
+            var data = db.Product.Find(id);
+            return View(data);
+        }
+
+        public ActionResult Update(int id)
+        {
+            var product = db.Product.Find(id);
+            product.ProductName += "!";
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult Add20Percent()
+        {
+            var data = db.Product.Where(p => p.ProductName.Contains("White"));
+
+            foreach (var item in data)
+            {
+
+                if (item.Price.HasValue)
+                    item.Price = item.Price.Value * 1.2m;
+
+            }
+
+            db.SaveChanges();
+            return RedirectToAction("Index");
+            
+        }
+    }
+    
+}
